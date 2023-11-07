92303d23cc3630e63255870a8b0dd05674f01d60
Akhil <akhilkumar4674@gmail.com>
2023-11-07 20:42:12 +0100

    added test codes with better function names

--- test_1.py
+++ test_1.py
@@ -8,45 +8,61 @@ def ros_init():
     rclpy.init()
 
 
-def test_arm_stowed_correctly_evaluated(ros_init):
-    # given
+    def test_arm_stowed_evaluation_when_arm_is_stowed_should_return_true(self):
+        # given
+        arm_stowed_values = [
+            -0.0001456737518310547,
+            -3.115017890930176,
+            0.0,
+            3.132694959640503,
+            1.570010781288147,
+            0.0025632381439208984,
+            -1.56974196434021,
+        ]
+        node = JointStateSubscriber()
+        # expected
+        assert node.is_arm_stowed(arm_stowed_values) is True
 
-    arm_stowed_values = [
-        -0.0001456737518310547,
-        -3.115017890930176,
-        0.0,
-        3.132694959640503,
-        1.570010781288147,
-        0.0025632381439208984,
-        -1.56974196434021,
-    ]
+    def test_arm_stowed_evaluation_when_arm_is_not_stowed_should_return_false(self):
+        
+        
 
-    node = JointStateSubscriber()
+        # given
+        arm_values = [0, 1, 2, 3, 4, 5, 6]
+        node = JointStateSubscriber()
+        # expected
+        assert node.is_arm_stowed(arm_values) is False
 
-    # then
+    def test_arm_stowed_evaluation_when_arm_is_not_stowed_values_should_return_false(self):
+        
+        
 
-    # expected
-    assert node.is_arm_in_stowed_position(arm_stowed_values) is True
+        #given
+        arm_values = [
+            -0.0001456737518310547,
+            -3.115017890930176,
+            0.0,
+            3.132694959640503,
+            1.570010781288147,
+            0.0025632381439208984,
+            -1.56974196434021,
+        ]
+        node = JointStateSubscriber()
+        #expected
+        assert node.is_arm_stowed(arm_values) is False
 
+    def test_gripper_evaluation_when_gripper_is_close_to_closed_position_should_return_true(self):
+        
+        # given
+        gripper_position = -0.02
+        node = JointStateSubscriber()
+        # expected
+        assert node.is_gripper_values(gripper_position) is True
 
-def test_gripper_open_correctly_evaluated(ros_init):
-    gripper_values = -0.05
+    def test_gripper_evaluation_when_gripper_is_close_to_open_position_should_return_false():
+    #given
+        gripper_position = 0.02
+        node = JointStateSubscriber()
+        # expected
+        assert node.is_gripper_values(gripper_position) is False
 
-    node = JointStateSubscriber()
-
-    assert node.is_position_within_gripper_range(gripper_values) is False
-
-
-def test_arm_not_stowed_correctly_evaluated(ros_init):
-    # given
-
-    arm_values = [0, 1, 2, 3, 4, 5, 6]
-
-    node = JointStateSubscriber()
-
-    # then
-
-    # expected
-    assert node.is_arm_stowed(arm_values) is False
-
-    
