f78f559ab21b6b947093d938b791c03dbe903042
Muhammad Ali <129994639+alibijarani@users.noreply.github.com>
2023-11-07 23:19:24 +0100

    Update modified_test_1.py
    Removed an extra function for arm_stowed false

--- modified_test_1.py
+++ modified_test_1.py
@@ -33,23 +33,6 @@ def ros_init():
         # expected
         assert node.is_arm_stowed(arm_values) is False
 
-    def test_arm_stowed_evaluation_when_arm_is_not_stowed_values_should_return_false(self):
-        
-        
-
-        #given
-        arm_values = [
-            -0.0001456737518310547,
-            -3.115017890930176,
-            0.0,
-            3.132694959640503,
-            1.570010781288147,
-            0.0025632381439208984,
-            -1.56974196434021,
-        ]
-        node = JointStateSubscriber()
-        #expected
-        assert node.is_arm_stowed(arm_values) is False
 
     def test_gripper_evaluation_when_gripper_is_close_to_closed_position_should_return_true(self):
         
